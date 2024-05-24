f2b141e29f5bc219832a1c92de1bfac4c293e88a
danielgvb <daniel.gutierrezvb@gmail.com>
2024-05-24 11:16:28 +0200

    more data and lasso and ridge regs

--- scripts/eda.r
+++ scripts/eda.r
@@ -11,6 +11,7 @@ library(readxl)
 library(dplyr) # for dataframe manipulation
 library(car) # for vif
 library(lmtest) # for breusch pagan test
+library(zoo)
 
 # change here for current data path
 data <- read_excel("C:/Users/danie/Documents/GitHub/unipd_sl_24/data/data.xlsx",sheet = "dataframe_col")
@@ -32,6 +33,24 @@ print(na_counts)
 
 # Construction and Industrial prod have nans, 
 # they are at the extremes so interpolation is not so nice
+
+# Interpolate finished_constructions
+# Interpolate NA values in the finished_constructions column
+# Interpolate internal NA values in the finished_constructions column
+data$finished_constructions <- na.approx(data$finished_constructions, na.rm = FALSE)
+
+# Fill leading NA values with the first non-NA value
+data$finished_constructions <- na.locf(data$finished_constructions, na.rm = FALSE, fromLast = FALSE)
+
+# Fill trailing NA values with the last non-NA value
+data$finished_constructions <- na.locf(data$finished_constructions, na.rm = FALSE, fromLast = TRUE)
+
+# Do lag of the dependent variable bc should be included as regressor
+data <- data %>%
+  mutate(y_1 = lag(y, n = 1))
+
+
+
 
 # Removing rows with any NA values
 clean_data <- na.omit(data)
@@ -55,6 +74,7 @@ View(data_reduced)
 
 # Outliers---------------
 
+# Hacer interquartil range
 # see outliers based on z scores
 
 df_with_z_scores <- data_reduced %>%
@@ -94,7 +114,7 @@ filtered_df_shipping <- clean_data %>%
 filtered_df_shipping
 
 # It was aug 2022-dec 2022, peak inflation wave, war on Ukraine and,
-#preocupation of winter in Europe, makes sense to leave it
+# preocupation of winter in Europe, makes sense to leave it
 
 # Create a pairs plot excluding columns C and D
 # pairs(data_reduced) # does not say much
@@ -109,7 +129,8 @@ colnames(data)
 # Select subset of variables you want to difference
 variables_to_calculate <- c("y", "industrial_inputs", "metals",
                             "energy", "shipping", "fx", "industrial_prod",
-                            "construction_licences_area", "google_trends")
+                            "construction_licences_area", "finished_constructions",
+                            "google_trends")
 
 
 # Create a function to calculate the 12-month percentual variation
@@ -123,32 +144,29 @@ data_percentual_variation <- data %>%
 
 View(data_percentual_variation)
 
-# Create new log-transformed variables
-data <- data %>%
-  mutate(across(c(y, industrial_inputs, metals, energy, shipping, fx,
-                  industrial_prod, construction_licences_area, google_trends),
-                list(log = ~log(.)), .names = "{.col}_log"))
 
 
 
 # Create new log-transformed variables
 data_reduced <- data_reduced %>%
-  mutate(across(c(y, industrial_inputs, metals, energy, shipping, fx,
-                  industrial_prod, construction_licences_area, google_trends),
+  mutate(across(c(y,y_1, industrial_inputs, metals, energy, shipping, fx,
+                  industrial_prod, construction_licences_area,finished_constructions,
+                  google_trends),
                 list(log = ~log(.)), .names = "{.col}_log"))
 
 # Subset the dataframe by selecting specific columns
 data_final <- data_reduced %>%
   select(y_log, industrial_inputs_log, metals_log, energy_log,
          shipping_log, fx_log,industrial_prod_log,
-         construction_licences_area_log, google_trends_log,
+         construction_licences_area_log, finished_constructions_log,google_trends_log,
          unemployment, interest_rate)
 
 # Subset only percentual changes
 data_percentual_variation <- data_percentual_variation %>%
   select(pct_var_y, pct_var_industrial_inputs, pct_var_metals, pct_var_energy,
          pct_var_shipping, pct_var_fx,pct_var_industrial_prod,
-         pct_var_construction_licences_area, pct_var_google_trends,
+         pct_var_construction_licences_area, pct_var_finished_constructions,
+         pct_var_google_trends,
          unemployment, interest_rate)
 
 # Ploting ----------------
@@ -218,10 +236,24 @@ par(mar = c(5, 4, 4, 2) + 0.1)
 
 # Covariance---------------
 
-cov_matrix <- cov(data_reduced)
+# Identify columns that do not contain "log"
+columns_to_keep <- !grepl("log", names(data_reduced))
+
+# Subset the dataframe to keep only these columns
+df_not_log <- data_reduced[, columns_to_keep]
+
+# Remove y_1
+df_not_log <- subset(df_not_log, select = -y_1)
+
+# Display the filtered dataframe
+print(df_not_log)
+
+
+
+cov_matrix <- cov(df_not_log)
 cov_matrix
 
-correl_matrix <- cor(data_reduced)
+correl_matrix <- cor(df_not_log)
 correl_matrix
 
 # Heatmap of correl
@@ -236,20 +268,18 @@ axis(2, at = 1:ncol(correl_matrix), labels = colnames(correl_matrix), las = 2)
 
 
 # Correlation matrix
-cor_matrix <- cor(data_reduced, use = "complete.obs")
+cor_matrix <- cor(df_not_log, use = "complete.obs")
 print(cor_matrix)
 
 # Visualize the correlation matrix
 heatmap(cor_matrix, symm = TRUE)
 
 
-
-
 # Multicolinearity Check--------------------
 
 # here a value bigger than 10 is supposed to have high multicolinearity 
-vif(lm(y_log ~ ., data = data_final))
-
+vif_values <-vif(lm(y_log ~ ., data = data_final))
+vif_values
 # some variables like shipping, industrial prod, metals and industrial input are high,
 # Metals and industrial inputs are very similar / metals is a part of industrial inputs,
 # drop metals
@@ -284,6 +314,8 @@ vif(lm(y_log ~ ., data = data_final))
 # Perform linear regression
 model <- lm(y_log ~ ., data = data_final)  # Replace 'Y' with the dependent variable name
 summary(model)
+
+# DO AIC or BIC to select variables also
 
 # Model on percentual vars
 #model <- lm(pct_var_y ~ ., data = data_percentual_variation)
@@ -329,14 +361,6 @@ print(ks_test)
 
 # Both p-values are > 0.05, so the residuals should be normal
 
-# Check for Homoskedasticity
-plot(fitted(model), residuals(model), main="Residuals vs Fitted")
-abline(h=0, col="red")
-
-# Breusch-Pagan Test
-
-bptest(model)
-# P value < 0.05 -> There is Heteroskedasticiy
 
 # Multicolinearity
 
@@ -358,40 +382,8 @@ plot(hatvalues(model), main="Leverage Values")
 abline(h = 2*mean(hatvalues(model)), col="red")  # Common threshold line
 
 
-# Autocorrelation plot
-# Durbin Watson
-dwtest(model)
-# H0 (null hypothesis): There is no correlation among the residuals.
-# p-value < 0.05 => reject H0, so there looks like there is serial correlation in residuals
-
-# For positive serial correlation, consider adding lags of the dependent and/or independent variable to the model.
-# For negative serial correlation, check to make sure that none of your variables are overdifferenced.
-# For seasonal correlation, consider adding seasonal dummy variables to the model.
-
-# Check type of autocorrelation
-
-# Plot ACF of residuals
-acf(residuals, main = "ACF of Regression Residuals")
-
-# Plot PACF of residuals
-pacf(residuals, main = "PACF of Regression Residuals")
-
-# According to these plots, we should include the lagged y in our model
-# to account for the ARIMA nature of the timeseries
-
-
-# Outliers
-std_res <- rstandard(model)
-plot(std_res, main="Standardized Residuals")
-abline(h=c(-2, 2), col="red")
-
-# There are are 3 values of outliers
-
-# Tests suggest normality in the residuals, but barely in the Shapiro Wilk 
+# Variable Selection-----------
 # F-test for the comparison of nested models
-
-# Reduce the model until it works
-
 full.mod <- lm(y_log ~ ., data = data_final)
 summary(full.mod)
 # Remove energy
@@ -404,6 +396,7 @@ anova(red.mod, full.mod)
 # A small p-value (typically < 0.05) suggests that the additional predictors
 # in the full model significantly improve the model fit compared 
 # to the reduced model.
+
 # Energy does not improve model
 summary(red.mod)
 # Remove industrial_inputs_log (only price variable so far)
@@ -413,12 +406,19 @@ anova(red.mod, red.mod2)
 summary(red.mod2)
 # All predictors are statistically significant
 
+# Remove finished_constructions_log (only price variable so far)
+red.mod3 <- update(red.mod2, . ~ . -finished_constructions_log)
+anova(red.mod2, red.mod3)
+# Can safely remove finished constructions (but makes no sense)
+summary(red.mod3)
+# All predictors are statistically significant
+
 # Post estimation 2--------------
 
-residuals_red     <- residuals(red.mod2)
+residuals_red     <- residuals(red.mod3)
 
 # Histogram of residuals
-hist(residuals(model), breaks = 30, main = "Residuals Histogram Reduced Model")
+hist(residuals(red.mod3), breaks = 30, main = "Residuals Histogram Reduced Model")
 
 # Plot QQ plot and histogram for residuals
 qqnorm(residuals_red, main = "QQ Plot of Residuals", col ='blue')
@@ -440,30 +440,276 @@ print(ks_test)
 # Both p-values are > 0.05, so the residuals should be normal
 
 # Check for Homoskedasticity
-plot(fitted(red.mod2), residuals(red.mod2), main="Residuals vs Fitted")
+plot(fitted(red.mod3), residuals(red.mod3), main="Residuals vs Fitted")
 abline(h=0, col="red")
 
 # Breusch-Pagan Test
 
-bptest(red.mod2)
+bptest(red.mod3)
 # P value < 0.05 -> There is Heteroskedasticiy
 
 # Multicolinearity
 
-vif(red.mod2)
+vif(red.mod3)
 # Shipping and Industrial production could be linearly correlated
 
 
 # Model fit
-summary(red.mod2)$r.squared
+summary(red.mod3)$r.squared
 
 
 # Influential plots
 # Cook's Distance Plot
-plot(cooks.distance(red.mod2), main="Cook's Distance")
-abline(h = 4/(nrow(data_final)-length(coef(red.mod2))), col="red")  # Common threshold line
+plot(cooks.distance(red.mod3), main="Cook's Distance")
+abline(h = 4/(nrow(data_final)-length(coef(red.mod3))), col="red")  # Common threshold line
 
 # Leverage Plot
-plot(hatvalues(red.mod2), main="Leverage Values")
-abline(h = 2*mean(hatvalues(red.mod2)), col="red")  # Common threshold line
+plot(hatvalues(red.mod3), main="Leverage Values")
+abline(h = 2*mean(hatvalues(red.mod3)), col="red")  # Common threshold line
+
+# Homoskedasticity
+
+
+# Check for Homoskedasticity
+plot(fitted(red.mod3), residuals(red.mod3), main="Residuals vs Fitted")
+abline(h=0, col="red")
+
+# Breusch-Pagan Test
+
+bptest(red.mod3)
+# P value < 0.05 -> There is Heteroskedasticiy
+# There seems to be homoskedasticity bc included y_1
+
+
+# Autocorrelation plot
+# Durbin Watson
+dwtest(red.mod3)
+# H0 (null hypothesis): There is no correlation among the residuals.
+# p-value < 0.05 => reject H0, so there looks like there is serial correlation in residuals
+
+# For positive serial correlation, consider adding lags of the dependent and/or independent variable to the model.
+# For negative serial correlation, check to make sure that none of your variables are overdifferenced.
+# For seasonal correlation, consider adding seasonal dummy variables to the model.
+
+# Check type of autocorrelation
+
+# Plot ACF of residuals
+acf(residuals_red, main = "ACF of Regression Residuals")
+
+# Plot PACF of residuals
+pacf(residuals_red, main = "PACF of Regression Residuals")
+
+# According to these plots, we should include the lagged y in our model
+# to account for the ARIMA nature of the timeseries
+
+
+# Outliers
+std_res <- rstandard(red.mod3)
+plot(std_res, main="Standardized Residuals")
+abline(h=c(-2, 2), col="red")
+
+# There are are 3 values of outliers
+
+# Model with lagged y-------------
+
+# Add lag of y to account for autocorrelation and heteroskedasticity
+# Subset the dataframe by selecting specific columns
+data_final <- data_reduced %>%
+  select(y_log, y_1_log, shipping_log, fx_log,
+         construction_licences_area_log,google_trends_log,
+         unemployment, interest_rate)
+
+
+model_lagged <- lm(y_log ~ ., data = data_final)  # Replace 'Y' with the dependent variable name
+summary(model_lagged)
+
+residuals_lagged    <- residuals(model_lagged)
+# Create a histogram with more customization
+hist(residuals_lagged, 
+     main = "Histogram of Residuals", 
+     xlab = "Residuals", 
+     ylab = "Frequency", 
+     col = "skyblue", 
+     border = "white",
+     breaks = 30)  # You can change the number of bins
+
+
+# Plot QQ plot and histogram for residuals
+qqnorm(residuals_lagged, main = "QQ Plot of Residuals", col ='blue')
+qqline(residuals_lagged, col = "red")
+# They look approximately normal
+
+# Do Test of normality to residuals
+
+# For all these tests, the null hypothesis H0  is that the residuals are normally distributed.
+# pvalue > 0.05 -> Fail to reject the null hypothesis, suggesting that the residuals are normally distributed.
+# Perform Shapiro-Wilk test
+shapiro_test <- shapiro.test(residuals_lagged)
+print(shapiro_test)
+
+# Perform Kolmogorov-Smirnov test
+ks_test <- ks.test(residuals_lagged, "pnorm", mean = mean(residuals_lagged), sd = sd(residuals_lagged))
+print(ks_test)
+
+# Both p-values are > 0.05, so the residuals should be normal
+
+# Check for Homoskedasticity
+plot(fitted(model_lagged), residuals(model_lagged), main="Residuals vs Fitted")
+abline(h=0, col="red")
+
+# Breusch-Pagan Test
+
+bptest(model_lagged)
+# P value < 0.05 -> There is Heteroskedasticiy
+
+# Multicolinearity
+
+vif(model_lagged)
+# Shipping and Industrial production could be linearly correlated
+
+
+# Model fit
+summary(model_lagged)$r.squared
+
+
+# Influential plots
+# Cook's Distance Plot
+plot(cooks.distance(model_lagged), main="Cook's Distance")
+abline(h = 4/(nrow(data_final)-length(coef(model_lagged))), col="red")  # Common threshold line
+
+# Leverage Plot
+plot(hatvalues(model_lagged), main="Leverage Values")
+abline(h = 2*mean(hatvalues(model_lagged)), col="red")  # Common threshold line
+
+# Homoskedasticity
+
+
+# Check for Homoskedasticity
+plot(fitted(model_lagged), residuals(model_lagged), main="Residuals vs Fitted")
+abline(h=0, col="red")
+
+# Breusch-Pagan Test
+
+bptest(model_lagged)
+# P value < 0.05 -> There is Heteroskedasticiy
+
+
+
+# Autocorrelation plot
+# Durbin Watson
+dwtest(model_lagged)
+# H0 (null hypothesis): There is no correlation among the residuals.
+# p-value < 0.05 => reject H0, so there looks like there is serial correlation in residuals
+
+# For positive serial correlation, consider adding lags of the dependent and/or independent variable to the model.
+# For negative serial correlation, check to make sure that none of your variables are overdifferenced.
+# For seasonal correlation, consider adding seasonal dummy variables to the model.
+
+# Check type of autocorrelation
+
+# Plot ACF of residuals
+acf(residuals_lagged, main = "ACF of Regression Residuals")
+
+# Plot PACF of residuals
+pacf(residuals_lagged, main = "PACF of Regression Residuals")
+
+# According to these plots, we should include the lagged y in our model
+# to account for the ARIMA nature of the timeseries
+
+
+# Outliers
+std_res <- rstandard(model_lagged)
+plot(std_res, main="Standardized Residuals")
+abline(h=c(-2, 2), col="red")
+
+
+
+# Regularization------------
+# Done over full model
+#install.packages("glmnet")
+#install.packages("caret")
+#install.packages("Metrics")
+
+
+
+# Load the packages
+library(glmnet)
+library(caret)
+library(Metrics)
+
+
+# Subset the dataframe by selecting specific columns
+data_final <- data_reduced %>%
+  select(y_log, industrial_inputs_log, metals_log, energy_log,
+         shipping_log, fx_log,industrial_prod_log,
+         construction_licences_area_log, finished_constructions_log,google_trends_log,
+         unemployment, interest_rate)
+
+# Sample data preparation
+# Assuming your data frame is `data_final` and your response variable is `y_log`
+set.seed(123)  # For reproducibility
+trainIndex <- createDataPartition(data_final$y_log, p = .8, 
+                                  list = FALSE, 
+                                  times = 1)
+data_train <- data_final[ trainIndex,]
+data_test  <- data_final[-trainIndex,]
+
+# Extract predictors and response
+X_train <- model.matrix(y_log ~ ., data_train)[, -1]  # Remove the intercept column
+y_train <- data_train$y_log
+
+X_test <- model.matrix(y_log ~ ., data_test)[, -1]  # Remove the intercept column
+y_test <- data_test$y_log
+
+# Fit linear regression model
+lm_model <- lm(y_log ~ ., data = data_train)
+
+# Predict and evaluate linear regression model
+lm_predictions <- predict(lm_model, newdata = data_test)
+lm_mse <- mse(y_test, lm_predictions)
+lm_r2 <- R2(y_test, lm_predictions)
+cat("Linear Regression MSE:", lm_mse, "\n")
+cat("Linear Regression R2:", lm_r2, "\n")
+
+# Fit LASSO regression model
+lasso_model <- cv.glmnet(X_train, y_train, alpha = 1)  # alpha = 1 for LASSO
+lasso_best_lambda <- lasso_model$lambda.min
+lasso_predictions <- predict(lasso_model, s = lasso_best_lambda, newx = X_test)
+lasso_mse <- mse(y_test, lasso_predictions)
+lasso_r2 <- R2(y_test, lasso_predictions)
+cat("LASSO Regression MSE:", lasso_mse, "\n")
+cat("LASSO Regression R2:", lasso_r2, "\n")
+
+# Coefficients at best lambda
+lasso_coefficients <- coef(lasso_model, s = lasso_best_lambda)
+print(lasso_coefficients)
+
+
+# Fit Ridge regression model
+ridge_model <- cv.glmnet(X_train, y_train, alpha = 0)  # alpha = 0 for Ridge
+ridge_best_lambda <- ridge_model$lambda.min
+ridge_predictions <- predict(ridge_model, s = ridge_best_lambda, newx = X_test)
+ridge_mse <- mse(y_test, ridge_predictions)
+ridge_r2 <- R2(y_test, ridge_predictions)
+cat("Ridge Regression MSE:", ridge_mse, "\n")
+cat("Ridge Regression R2:", ridge_r2, "\n")
+
+# Coefficients at best lambda
+ridge_coefficients <- coef(ridge_model, s = ridge_best_lambda)
+print(ridge_coefficients)
+
+# Compare the results
+results <- data.frame(
+  Model = c("Linear Regression", "LASSO Regression", "Ridge Regression"),
+  MSE = c(lm_mse, lasso_mse, ridge_mse),
+  R2 = c(lm_r2, lasso_r2, ridge_r2)
+)
+
+print(results)
+
+# Meter el cemento (esperando refinitiv)
+# Meter regression Lasso, Ridge (OK)
+# Meter AIC y BIC backward and forward selection
+# Meter un modelo generalizado de errores no normales
+# Montar un Notebook
 
